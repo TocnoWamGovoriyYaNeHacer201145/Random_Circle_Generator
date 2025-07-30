ef15ece06e9f0da2d9238bab33e3b9c8d6f8ec3c
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-07-30 17:40:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from turtle import *
-import turtle
 from random import *
 mode = input('Enter mode (random/custom): ')
 if mode == 'custom':
     lr_settings = int(input('Enter maximum left and right value: '))
     fb_settings = int(input('Enter maximum forward and backward value: '))
     # Turtle setup
-    turtle.color("black")
-    turtle.speed(0)
-    turtle.home()
+    color("black")
+    speed(0)
+    home()
     hideturtle()
     # Random forward and left movement setup
     f, l, r, b = randrange(-80, fb_settings), randrange(-360, lr_settings), randrange(-80, fb_settings), randrange(-80, fb_settings)
@@ -20,15 +19,12 @@ if mode == 'custom':
         backward(b)
 elif mode == 'random':
     # Turtle setup
-    turtle.color("black")
-    turtle.speed(0)
-    turtle.home()
+    color("black")
+    speed(0)
+    home()
     hideturtle()
     # Random forward and left movement setup
-    f = randrange(-80, 100)
-    l = randrange(-360, 360)
-    r = randrange(-360, 360)
-    b = randrange(-80, 100)
+    f, l, r, b = randrange(-80, 100), randrange(-360, 360), randrange(-360, 360), randrange(-80, 100)
     rrange = randint(10000, 1000000)
     for i in range(rrange):
         forward(f)
